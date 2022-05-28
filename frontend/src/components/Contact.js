cf0c2fcf02211ecf841e5c9558e9cf358ec58d52
Anthony Modica <modica.c.anthony@gmail.com>
2022-05-27 19:45:35 -0500

    Beginning redesign of contact page

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -3,7 +3,7 @@ import '../styles/Contact.css';
 
 const Contact = () =>  {
     return (
-        <div>
+        <div className="main-container">
             <Typography variant="h2" align="center" gutterBottom>
                 Contact Us
             </Typography>
@@ -14,14 +14,14 @@ const Contact = () =>  {
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">President</div>
-                Bryce Hall
+                []
                 <br></br>
                 president@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Administrative Vice President</div>
-                Taylor Himes
+                []
                 <br></br>
                 avp@texasapo.org
             </Typography>
@@ -29,35 +29,35 @@ const Contact = () =>  {
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Leadership Vice President</div>
-                Roxanne Springman
+                []
                 <br></br>
                 lvp@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Membership Vice President</div>
-                Julio Ramos
+                []
                 <br></br>
                 mvp@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Pledge Trainer</div>
-                Lucas Best
+                []
                 <br></br>
                 pt@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Service Vice President</div>
-                Cameron Dickison
+                []
                 <br></br>
                 svp@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Treasurer</div>
-                Marco Mejia
+                []
                 <br></br>
                 treasurer@texasapo.org
             </Typography>
@@ -74,21 +74,21 @@ const Contact = () =>  {
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Advisor-Alumni Liaison</div>
-                Sammy Calkins
+                []
                 <br></br>
                 alumni@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Interchapter Relations Chair</div>
-                Lawrence Robinson
+                []
                 <br></br>
                 erc@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Flag Coordinator</div>
-                Elliana Garcia and Robert Resendez
+                []
                 <br></br>
                 flag@texasapo.org
             </Typography>
@@ -101,28 +101,28 @@ const Contact = () =>  {
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Historian</div>
-                Rachel West
+                []
                 <br></br>
                 historian@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Public Relations Chair</div>
-                Selene Cullen
+                []
                 <br></br>
                 pr@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Webmaster</div>
-                Sammy Shin
+                []
                 <br></br>
                 webmaster@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Chapter Communications Chair</div>
-                Emily Nulisch and Max Truty
+                []
                 <br></br>
                 webmaster@texasapo.org
             </Typography>
@@ -135,14 +135,14 @@ const Contact = () =>  {
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Chapter Development Chair</div>
-                Addie Costello and Alyssa Edwards
+                []
                 <br></br>
                 cdc@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Professional Development Chair</div>
-                Adrienne Brown
+                []
                 <br></br>
                 ppdc@texasapo.org
             </Typography>
@@ -155,28 +155,28 @@ const Contact = () =>  {
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Active Retention Chair</div>
-                Claire Schmidt and Sean Hart
+                []
                 <br></br>
                 arc@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Fellowship Chair</div>
-                Raul Deleon and Kendra Crowl
+                []
                 <br></br>
                 fellowship@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Recruitment Coordinator</div>
-                Nicole Jackson and Raul Deleon
+                []
                 <br></br>
                 recruitment@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Requirements Chair</div>
-                Samid Mirza
+                []
                 <br></br>
                 requirements.chair@texasapo.org
             </Typography>
@@ -189,7 +189,7 @@ const Contact = () =>  {
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Old Couple</div>
-                Ali Gentry and Carson Lee
+                []
                 <br></br>
                 oldcouple@texasapo.org
             </Typography>
@@ -202,42 +202,42 @@ const Contact = () =>  {
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Blood Drive Coordinator</div>
-                Jenna Dent
+                []
                 <br></br>
                 blood.drive@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Campus Projects Director</div>
-                Daniel Guarnero
+                []
                 <br></br>
                 campus@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Community Projects Director</div>
-                Katia Davis
+                []
                 <br></br>
                 community@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Girl Scout University Coordinator</div>
-                Kjerstin Hall and Justice Kaigler
+                []
                 <br></br>
                 texasgsu@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Merit Badge University Coordinator</div>
-                Sebastian Davila and Samuel Gillette
+                []
                 <br></br>
                 mbu@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Scouting and Youth Director</div>
-                Katrina Benjamin and Victoria de la Garza
+                []
                 <br></br>
                 scouting@texasapo.org
             </Typography>
@@ -250,14 +250,14 @@ const Contact = () =>  {
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Assistant Treasurer</div>
-                Marco Mejia
+                []
                 <br></br>
                 assistant.treasurer@texasapo.org
             </Typography>
 
             <Typography variant="body1" gutterBottom>
                 <div className="position-title">Merchandise Chair</div>
-                Shaena Reyes and Alyssa Garcia
+                []
                 <br></br>
                 merchandise@texasapo.org
             </Typography>
