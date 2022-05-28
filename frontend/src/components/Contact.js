7c57f9e47f5d7b747bde5d303f50bff8e567ad90
Anthony Modica <modica.c.anthony@gmail.com>
2022-05-27 19:31:04 -0500

    Styled content on Contact page

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,4 +1,5 @@
 import Typography from '@mui/material/Typography';
+import '../styles/Contact.css';
 
 const Contact = () =>  {
     return (
@@ -7,135 +8,259 @@ const Contact = () =>  {
                 Contact Us
             </Typography>
 
-            <Typography variant="h4">
+            <Typography variant="h5" gutterBottom>
                 Fall 2022 Executive Board
             </Typography>
 
-            President
-            Bryce Hall
-            president@texasapo.org
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">President</div>
+                Bryce Hall
+                <br></br>
+                president@texasapo.org
+            </Typography>
 
-            Administrative Vice President
-            Taylor Himes
-            avp@texasapo.org
-
-            Leadership Vice President
-            Roxanne Springman
-            lvp@texasapo.org
-
-            Membership Vice President
-            Julio Ramos
-            mvp@texasapo.org
-
-            Pledge Trainer
-            Lucas Best
-            pt@texasapo.org
-
-            Service Vice President
-            Cameron Dickison
-            svp@texasapo.org
-
-            Treasurer
-            Marco Mejia
-            treasurer@texasapo.org
-
-            Spring 2020 Officer Board
-            Presidential Branch
-            Advisor-Alumni Liaison
-            Sammy Calkins
-            alumni@texasapo.org
-
-            Interchapter Relations Chair
-            Lawrence Robinson
-            erc@texasapo.org
-
-            Flag Coordinator
-            Elliana Garcia and Robert Resendez
-            flag@texasapo.org
-
-            Administrative Branch
-            Historian
-            Rachel West
-            historian@texasapo.org
-
-            Public Relations Chair
-            Selene Cullen
-            pr@texasapo.org
-
-            Webmaster
-            Sammy Shin
-            webmaster@texasapo.org
-
-            Chapter Communications Chair
-            Emily Nulisch and Max Truty
-            webmaster@texasapo.org
-
-            Leadership Branch
-            Chapter Development Chair
-            Addie Costello and Alyssa Edwards
-            cdc@texasapo.org
-
-            Personal/Professional Development Chair
-            Adrienne Brown
-            ppdc@texasapo.org
-
-            Membership Branch
-            Active Retention Chair
-            Claire Schmidt and Sean Hart
-            arc@texasapo.org
-
-            Fellowship Chair
-            Raul Deleon and Kendra Crowl
-            fellowship@texasapo.org
-
-            Recruitment Coordinator
-            Nicole Jackson and Raul Deleon
-            recruitment@texasapo.org
-
-            Requirements Chair
-            Samid Mirza
-            requirements.chair@texasapo.org
-
-            Pledge Program
-            Old Couple
-            Ali Gentry and Carson Lee
-            oldcouple@texasapo.org
-
-            Service Branch
-            Blood Drive Coordinator
-            Jenna Dent
-            blood.drive@texasapo.org
-
-            Campus Projects Director
-            Daniel Guarnero
-            campus@texasapo.org
-
-            Community Projects Director
-            Katia Davis
-            community@texasapo.org
-
-            Girl Scout University Coordinator
-            Kjerstin Hall and Justice Kaigler
-            texasgsu@texasapo.org
-
-            Merit Badge University Coordinator
-            Sebastian Davila and Samuel Gillette
-            mbu@texasapo.org
-
-            Scouting and Youth Director
-            Katrina Benjamin and Victoria de la Garza
-            scouting@texasapo.org
-
-            Treasury Branch
-            Assistant Treasurer
-            Marco Mejia
-            assistant.treasurer@texasapo.org
-
-            Merchandise Chair
-            Shaena Reyes and Alyssa Garcia
-            merchandise@texasapo.org
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Administrative Vice President</div>
+                Taylor Himes
+                <br></br>
+                avp@texasapo.org
+            </Typography>
 
 
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Leadership Vice President</div>
+                Roxanne Springman
+                <br></br>
+                lvp@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Membership Vice President</div>
+                Julio Ramos
+                <br></br>
+                mvp@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Pledge Trainer</div>
+                Lucas Best
+                <br></br>
+                pt@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Service Vice President</div>
+                Cameron Dickison
+                <br></br>
+                svp@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Treasurer</div>
+                Marco Mejia
+                <br></br>
+                treasurer@texasapo.org
+            </Typography>
+
+            <br></br>
+
+            <Typography variant="h5" gutterBottom>
+                Fall 2022 Officer Board
+            </Typography>
+
+            <Typography variant="caption">
+                Presidential Branch
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Advisor-Alumni Liaison</div>
+                Sammy Calkins
+                <br></br>
+                alumni@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Interchapter Relations Chair</div>
+                Lawrence Robinson
+                <br></br>
+                erc@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Flag Coordinator</div>
+                Elliana Garcia and Robert Resendez
+                <br></br>
+                flag@texasapo.org
+            </Typography>
+
+            <br></br>
+
+            <Typography variant="caption">
+                Administrative Branch
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Historian</div>
+                Rachel West
+                <br></br>
+                historian@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Public Relations Chair</div>
+                Selene Cullen
+                <br></br>
+                pr@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Webmaster</div>
+                Sammy Shin
+                <br></br>
+                webmaster@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Chapter Communications Chair</div>
+                Emily Nulisch and Max Truty
+                <br></br>
+                webmaster@texasapo.org
+            </Typography>
+
+            <br></br>
+
+            <Typography variant="caption">
+                Leadership Branch
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Chapter Development Chair</div>
+                Addie Costello and Alyssa Edwards
+                <br></br>
+                cdc@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Professional Development Chair</div>
+                Adrienne Brown
+                <br></br>
+                ppdc@texasapo.org
+            </Typography>
+
+            <br></br>
+
+            <Typography variant="caption">
+                Membership Branch
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Active Retention Chair</div>
+                Claire Schmidt and Sean Hart
+                <br></br>
+                arc@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Fellowship Chair</div>
+                Raul Deleon and Kendra Crowl
+                <br></br>
+                fellowship@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Recruitment Coordinator</div>
+                Nicole Jackson and Raul Deleon
+                <br></br>
+                recruitment@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Requirements Chair</div>
+                Samid Mirza
+                <br></br>
+                requirements.chair@texasapo.org
+            </Typography>
+
+            <br></br>
+
+            <Typography variant="caption">
+                Pledge Program
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Old Couple</div>
+                Ali Gentry and Carson Lee
+                <br></br>
+                oldcouple@texasapo.org
+            </Typography>
+
+            <br></br>
+
+            <Typography variant="caption">
+                Service Branch
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Blood Drive Coordinator</div>
+                Jenna Dent
+                <br></br>
+                blood.drive@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Campus Projects Director</div>
+                Daniel Guarnero
+                <br></br>
+                campus@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Community Projects Director</div>
+                Katia Davis
+                <br></br>
+                community@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Girl Scout University Coordinator</div>
+                Kjerstin Hall and Justice Kaigler
+                <br></br>
+                texasgsu@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Merit Badge University Coordinator</div>
+                Sebastian Davila and Samuel Gillette
+                <br></br>
+                mbu@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Scouting and Youth Director</div>
+                Katrina Benjamin and Victoria de la Garza
+                <br></br>
+                scouting@texasapo.org
+            </Typography>
+
+            <br></br>
+
+            <Typography variant="caption">
+                Treasury Branch
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Assistant Treasurer</div>
+                Marco Mejia
+                <br></br>
+                assistant.treasurer@texasapo.org
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <div className="position-title">Merchandise Chair</div>
+                Shaena Reyes and Alyssa Garcia
+                <br></br>
+                merchandise@texasapo.org
+            </Typography>
         </div>
     );
 }
