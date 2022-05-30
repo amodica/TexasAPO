2743ed7911d6b956866d2e42a616d249955d0065
Anthony Modica <modica.c.anthony@gmail.com>
2022-05-29 20:31:00 -0500

    Styling for Flag page

--- frontend/src/components/Flag.js
+++ frontend/src/components/Flag.js
@@ -1,16 +1,23 @@
+import Typography from '@mui/material/Typography';
+
 const Flag = () => {
     return (
         <div>
-            <h1>THE WORLD’S LARGEST TEXAS FLAG</h1>
-            ABOUT THE TEXAS FLAG
-The world’s largest Texas flag is run by members of Alpha Phi Omega at many different UT sporting events and celebrations. During the pre-game festivities at every home football game, and at the annual Texas-OU game at the Cotton Bowl in Dallas, the Texas Flag is seen on the field. Additionally, the Texas Flag appears at several home basketball games in the spring. The Texas flag can also be seen being marched down The Drag during the annual OU Torchlight Parade and hung from the president’s balcony for special events such as Gone to Texas or Forty Acres Fest. APO members take great pride in the history and tradition of the Texas flag. We not only think of the Texas flag as one of our traditions, but as a tradition of the university and the great state of Texas.
 
- 
+            <Typography variant="h3">
+                The World's Largest Texas Flag
+            </Typography>
 
-HISTORY OF THE TEXAS FLAG
-The Alpha Rho chapter of Alpha Phi Omega is currently in possession of the world’s three largest Texas flags. Each flag is used during different events. However, there have been a total of eight Texas flags throughout APO’s history. Alpha Rho also currently owns an American flag that is brought out for special occasions. The original flag was given to Texas governor Price Daniel by Mississippi governor Ross Barnett during the halftime show of the 1962 Cotton Bowl (after which Texas beat Ole Miss 12-7). The flag was meant as a gesture of southern hospitality from the citizens of Mississippi to the citizens of Texas. Governor Daniel later gave the flag to the Longhorn Band. The band later, however, returned the flag to the university where it eventually made its way to the men’s athletic department. The next football season, APO was asked to run the flag and the first APO flag run was at the Thanksgiving day game in 1962 against Texas A&M, a game which UT won. The original Texas flag measured 17 yards by 30 yards (51 feet by 90 feet). Within a few years we needed a new flag. The second Texas flag was made and came into APO’s possession around 1966. Unfortunately, not much is known about this flag because the records have not survived. It is assumed to be roughly the same size as the first flag. Our current field flag was made in 2007, our parade flag in 1998, and our drop flag was made in 1991. All flags, including those not in use, are stored in our custom trailer, which is safely stored by the UT athletic department at Darrell K Royal Texas Memorial Stadium.
+            <div>
+                <p>
+                    The world’s largest Texas flag is run by members of Alpha Phi Omega at many different UT sporting events and celebrations. During the pre-game festivities at every home football game, and at the annual Texas-OU game at the Cotton Bowl in Dallas, the Texas Flag is seen on the field. Additionally, the Texas Flag appears at several home basketball games in the spring. The Texas flag can also be seen being marched down The Drag during the annual OU Torchlight Parade and hung from the president’s balcony for special events such as Gone to Texas or Forty Acres Fest. APO members take great pride in the history and tradition of the Texas flag. We not only think of the Texas flag as one of our traditions, but as a tradition of the university and the great state of Texas.
 
-The Texas Flag has made appearances at two National Championship games in 2005 and again in 2010.
+                    HISTORY OF THE TEXAS FLAG
+                    The Alpha Rho chapter of Alpha Phi Omega is currently in possession of the world’s three largest Texas flags. Each flag is used during different events. However, there have been a total of eight Texas flags throughout APO’s history. Alpha Rho also currently owns an American flag that is brought out for special occasions. The original flag was given to Texas governor Price Daniel by Mississippi governor Ross Barnett during the halftime show of the 1962 Cotton Bowl (after which Texas beat Ole Miss 12-7). The flag was meant as a gesture of southern hospitality from the citizens of Mississippi to the citizens of Texas. Governor Daniel later gave the flag to the Longhorn Band. The band later, however, returned the flag to the university where it eventually made its way to the men’s athletic department. The next football season, APO was asked to run the flag and the first APO flag run was at the Thanksgiving day game in 1962 against Texas A&M, a game which UT won. The original Texas flag measured 17 yards by 30 yards (51 feet by 90 feet). Within a few years we needed a new flag. The second Texas flag was made and came into APO’s possession around 1966. Unfortunately, not much is known about this flag because the records have not survived. It is assumed to be roughly the same size as the first flag. Our current field flag was made in 2007, our parade flag in 1998, and our drop flag was made in 1991. All flags, including those not in use, are stored in our custom trailer, which is safely stored by the UT athletic department at Darrell K Royal Texas Memorial Stadium.
+
+                    The Texas Flag has made appearances at two National Championship games in 2005 and again in 2010.
+                </p>
+            </div>
         </div>
     );
 }
