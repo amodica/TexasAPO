449be83a9d176249c5083778325fe12c2f6dde4f
Anthony Modica <modica.c.anthony@gmail.com>
2022-05-29 20:29:04 -0500

    Styling for History page

--- frontend/src/components/History.js
+++ frontend/src/components/History.js
@@ -1,15 +1,21 @@
+import Typography from '@mui/material/Typography';
+
 const History = () => {
     return (
         <div>
-            <h1>HISTORY OF TEXAS APO</h1>
-            HISTORY OF TEXAS APO
-On March 11, 1935, thirty men met for the purpose of forming a chapter of Alpha Phi Omega at the University of Texas at Austin.  Frank Dominguez III wrote to the national office concerning a petition of affiliation and received information about the fraternity and the requirements to establish a chapter. Shortly later, the Alpha Rho chapter of Alpha Phi Omega was chartered.
+            <Typography variant="h2">
+                History of Texas APO
+            </Typography>
 
-In the years since, the Alpha Rho chapter has grown to become the premier service organization at The University of Texas at Austin. The chapter welcomed its first co-ed pledge class in 1976.  From running the worlds largest Texas flag, to running the campus’s largest blood drive, the Alpha Rho chapter of Alpha Phi Omega is deeply committed to serving the University of Texas at Austin and the surrounding community.
+            <div>
+                <p>
+                    On March 11, 1935, thirty men met for the purpose of forming a chapter of Alpha Phi Omega at the University of Texas at Austin.  Frank Dominguez III wrote to the national office concerning a petition of affiliation and received information about the fraternity and the requirements to establish a chapter. Shortly later, the Alpha Rho chapter of Alpha Phi Omega was chartered.
 
- 
+                    In the years since, the Alpha Rho chapter has grown to become the premier service organization at The University of Texas at Austin. The chapter welcomed its first co-ed pledge class in 1976.  From running the worlds largest Texas flag, to running the campus’s largest blood drive, the Alpha Rho chapter of Alpha Phi Omega is deeply committed to serving the University of Texas at Austin and the surrounding community.
 
-If you wish to know more about our history, you contact our historian at historian@texasapo.org 
+                    If you wish to know more about our history, you contact our historian at historian@texasapo.org 
+                </p>
+            </div>
         </div>
     );
 }
